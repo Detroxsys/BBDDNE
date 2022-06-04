2c8ecb76659d994b86d686552ed28620f3a80b6f
Detroxsys <yeudiellm@ciencias.unam.mx>
2022-06-04 17:33:44 -0500

    sql
    datos y diseño (preeliminar de sql)

---
+++ Proyecto_Final/relacional.sql
@@ -0,0 +1,73 @@
+/*Para borrrar la base de datos*/
+DROP DATABASE IF EXISTS panaderia;
+CREATE DATABASE  panaderia;
+USE panaderia;
+/*Sólo en MYSQL podemos usar USE para cambiar de base de datos*/
+
+
+/*CORTES DE MEDALLAS SEGÚN EL AÑO*/
+DROP TABLE IF EXISTS producto;
+CREATE TABLE producto(
+	id int(3) NOT NULL AUTO_INCREMENT, 
+    nombre varchar(30), 
+    precio_unit double, 
+    descripcion text(500), 
+    categoria varchar(30), 
+    cantidad_disp int(5), 
+    PRIMARY KEY(id)
+); 
+
+DROP TABLE IF EXISTS insumo;
+CREATE TABLE insumo(
+	id int(3) NOT NULL AUTO_INCREMENT, 
+    nombre varchar(30), 
+    precio_unit double, 
+    descripcion text(500), 
+    categoria varchar(30), 
+    cantidad_disp int(5), 
+    PRIMARY KEY(id)
+); 
+
+DROP TABLE IF EXISTS trabajadores;
+CREATE TABLE trabajadores(
+	curp varchar(20),
+    es_admin int(1),  
+    nombre varchar(30), 
+    apellidos varchar(60), 
+    contraseña varchar(10),
+    PRIMARY KEY(curp)
+); 
+
+
+DROP TABLE IF EXISTS vendedores_asociados;
+CREATE TABLE vendedores_asociados(
+	curp varchar(20),
+    nombre varchar(30), 
+    apellidos varchar(60), 
+    descuento  double,
+    PRIMARY KEY(curp)
+); 
+
+DROP TABLE IF EXISTS reporte; 
+CREATE TABLE reporte(
+	dia date,
+    gastos_extras double, 
+    ingresos_extras double, 
+    ingresos_tienda double, 
+    ingresos_vendedores double, 
+    ingresos_total double, 
+    ganancias_virtual double, 
+    ganancias_real double, 
+    perdidas_prop double,
+    PRIMARY KEY(dia)
+); 
+
+
+
+
+
+
+
+
+
+
