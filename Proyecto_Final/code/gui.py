0e25de7f8aba7dba2194617b550265dfe03ad8bc
Detroxsys <yeudiellm@ciencias.unam.mx>
2022-06-05 18:25:21 -0500

    gui

--- Proyecto_Final/code/gui.py
+++ Proyecto_Final/code/gui.py
@@ -1,7 +1,11 @@
+#Interfaz Grafica 
 from tkinter import* #pip install tk 
 from tkinter import ttk
 from PIL import Image, ImageTk  #pip install pillow
 from tkinter import messagebox
+from tkinter import simpledialog
+
+#Python Manipulations
 import pandas as pd 
 import numpy as np
 from pandastable import Table, TableModel 
@@ -18,7 +22,7 @@ class Login_window(Frame):
     def __init__(self, root):
         self.root=root
         #self.root.wm_attributes("-transparentcolor", 'black')
-        self.root.title("Login")
+        self.root.title("EL VOLOVAN")
         self.root.geometry("1550x800+0+0")
         bg = Image.open("img/background.jpg")
         bg = bg.resize( (1550, 800), Image.ANTIALIAS)
@@ -66,7 +70,6 @@ class Login_window(Frame):
 
     def register_window(self): 
         self.root.switch_frame(Register_window)
-        pass
 
     def login(self): 
         if self.txtuser.get()=="":
@@ -89,7 +92,7 @@ class Register_window(Frame):
     def __init__(self, root):
         self.root=root
         #self.root.wm_attributes("-transparentcolor", 'black')
-        self.root.title("Login")
+        self.root.title("EL VOLOVAN")
         self.root.geometry("1550x800+0+0")
         bg = Image.open("img/background.jpg")
         bg = bg.resize( (1550, 800), Image.ANTIALIAS)
@@ -160,7 +163,7 @@ class Admin_window(Frame):
     def __init__(self, root):
         self.root=root
         #self.root.wm_attributes("-transparentcolor", 'black')
-        self.root.title("Login")
+        self.root.title("EL VOLOVAN")
         self.root.geometry("1550x800+0+0")
         bg = Image.open("img/background.jpg")
         bg = bg.resize( (1550, 800), Image.ANTIALIAS)
@@ -179,23 +182,30 @@ class Admin_window(Frame):
         lbllogo=Label(frame, image=self.photoImage1, bg="white", borderwidth=0)
         lbllogo.place(x=20, y=20, width=200, height=200)
 
-
-
         get_str = Label(frame, text="Opciones\npara Admin", font =("sans-serif", 50, "bold"), fg="darkorange", bg="white")
         get_str.place(x=225, y=45)
         
         #Label 
+        initdaybtn = Button(frame,command=self.gotoInitDay, text="Iniciar Día",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        initdaybtn.place(x=125, y= 230, width=150, height=50)
+
+        enddaybtn = Button(frame,command=self.gotoEndDay, text="Finalizar Día",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        enddaybtn.place(x=325, y= 230, width=150, height=50)
+
+
         registerBoxbtn = Button(frame,command=self.gotoRegisterBox, text="Usar Caja",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        registerBoxbtn.place(x=125, y= 230, width=150, height=50)
+        registerBoxbtn.place(x=125, y= 290, width=150, height=50)
 
         makeReportbtn = Button(frame,command=self.gotoMakeReport, text="Hacer Reporte",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        makeReportbtn.place(x=125, y= 300, width=150, height=50)
-
-        newProductbtn = Button(frame,command=self.gotoPriviliges, text="Dar Privilegios",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        newProductbtn.place(x=325, y= 230, width=150, height=50)
+        makeReportbtn.place(x=325, y= 290, width=150, height=50)
 
         newProductbtn = Button(frame,command=self.gotoNewProduct, text="Nuevo Producto",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        newProductbtn.place(x=325, y= 300, width=150, height=50)
+        newProductbtn.place(x=125, y= 350, width=150, height=50)
+
+        produccionbtn = Button(frame,command=self.production, text="Producción",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        produccionbtn.place(x=325, y= 350, width=150, height=50)
+
+
 
         exitbtn = Button(frame,command=self.exit, text="Salir",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
         exitbtn.place(x=15, y= 420, width=100, height=50)
@@ -207,10 +217,16 @@ class Admin_window(Frame):
     def gotoMakeReport(self): 
         pass
 
-    def gotoPriviliges(self): 
+    def gotoNewProduct(self): 
+        self.root.switch_frame(New_Product_window)
+
+    def production(self):
+        self.root.switch_frame(Production_window)
+
+    def gotoInitDay(self): 
         pass
 
-    def gotoNewProduct(self): 
+    def gotoEndDay(self): 
         pass
 
     def exit(self): 
@@ -221,7 +237,7 @@ class RegisterBox_window(Frame):
     def __init__(self, root):
         self.root=root
         #self.root.wm_attributes("-transparentcolor", 'black')
-        self.root.title("Login")
+        self.root.title("EL VOLOVAN")
         self.root.geometry("1550x800+0+0")
         bg = Image.open("img/background.jpg")
         bg = bg.resize( (1550, 800), Image.ANTIALIAS)
@@ -245,16 +261,25 @@ class RegisterBox_window(Frame):
         
         #Label 
         cajaStorebtn = Button(frame,command=self.gotoVentas, text="Ir a Caja",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        cajaStorebtn.place(x=125, y= 230, width=150, height=50)
+        cajaStorebtn.place(x=50, y= 230, width=150, height=50)
 
-        pedidosbtn = Button(frame,command=self.gotoPedidos, text="Hacer Pedidos",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        pedidosbtn.place(x=125, y= 300, width=150, height=50)
+        makePedidosbtn = Button(frame,command=self.gotoMakePedidos, text="Hacer Pedidos",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        makePedidosbtn.place(x=225, y= 230, width=150, height=50)
+
+        getPedidosbtn = Button(frame,command=self.gotoGetPedidos, text="Ver Entregas",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        getPedidosbtn.place(x=400, y= 230, width=150, height=50)
 
         extraIngbtn = Button(frame,command=self.gotoIngresos, text="Ingreso Extra",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        extraIngbtn.place(x=325, y= 230, width=150, height=50)
+        extraIngbtn.place(x=125, y= 290, width=150, height=50)
 
         extraGastbtn = Button(frame,command=self.gotoGastos, text="Gasto Extra",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
-        extraGastbtn.place(x=325, y= 300, width=150, height=50)
+        extraGastbtn.place(x=325, y= 290, width=150, height=50)
+
+        checkIngbtn = Button(frame,command=self.df_checkIngresos, text="Revisar Ingreso Extra",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        checkIngbtn.place(x=75, y= 350, width=200, height=50)
+
+        checkGastbtn = Button(frame,command=self.df_checkGastos, text="Revisar Gasto Extra",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        checkGastbtn.place(x=325, y= 350, width=200, height=50)
 
         exitbtn = Button(frame,command=self.exit, text="Salir",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
         exitbtn.place(x=15, y= 420, width=100, height=50)
@@ -263,18 +288,155 @@ class RegisterBox_window(Frame):
     def gotoVentas(self): 
         pass
 
-    def gotoPedidos(self): 
-        pass
-
     def gotoIngresos(self): 
         pass
 
     def gotoGastos(self): 
         pass
 
+    def df_checkIngresos(self): 
+        pass
+
+    def df_checkGastos(self): 
+        pass
+
+    def gotoMakePedidos(self): 
+        pass
+
+    def gotoGetPedidos(self): 
+        pass
+
     def exit(self): 
         self.root.switch_frame(Login_window)
 
+
+#Registrar Nuevo Usuario
+class New_Product_window(Frame):
+    def __init__(self, root):
+        self.root=root
+        #self.root.wm_attributes("-transparentcolor", 'black')
+        self.root.title("EL VOLOVAN")
+        self.root.geometry("1550x800+0+0")
+        bg = Image.open("img/background.jpg")
+        bg = bg.resize( (1550, 800), Image.ANTIALIAS)
+        self.bg = ImageTk.PhotoImage(bg)
+        lbl_bg  = Label(self.root, image= self.bg)
+        lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
+
+        Frame.__init__(self, root)
+        frame = Frame(self.root, bg="white")
+        frame.place(x=250, y=180, width= 900, height=500)
+
+
+        logo = Image.open("img/logo.png")
+        logo = logo.resize((200,200), Image.ANTIALIAS)
+        self.photoImage1= ImageTk.PhotoImage(logo)
+        lbllogo=Label(frame, image=self.photoImage1, bg="white", borderwidth=0)
+        lbllogo.place(x=20, y=20, width=200, height=200)
+
+
+
+        get_str = Label(frame, text="Nuevo\nProducto", font =("sans-serif", 50, "bold"), fg="darkorange", bg="white")
+        get_str.place(x=250, y=40)
+        
+        #Label 
+        name=Label(frame, text="Nombre", font =("sans-serif", 20, "bold"), fg="teal", bg="white")
+        name.place(x=20, y=240)
+
+        self.txtname = ttk.Entry(frame, font =("sans-serif", 20, "bold"))
+        self.txtname.place(x=170, y=240, width=250)
+
+        description=Label(frame, text="Descripción", font =("sans-serif", 20, "bold"), fg="teal", bg="white")
+        description.place(x=450, y=240)
+
+        self.txtdescription = ttk.Entry(frame, font =("sans-serif", 20, "bold"))
+        self.txtdescription.place(x=620, y=240, width=250)
+        
+        precioUnitario=Label(frame, text="Precio", font =("sans-serif", 20, "bold"), fg="teal", bg="white")
+        precioUnitario.place(x=20, y=320)
+
+        self.txtprecioUnitario = ttk.Entry(frame, font =("sans-serif", 20, "bold"))
+        self.txtprecioUnitario.place(x=170, y=320, width=250)
+
+        categoria=Label(frame, text="Categoria", font =("sans-serif", 20, "bold"), fg="teal", bg="white")
+        categoria.place(x=450, y=320)
+
+        self.txtcategoria = ttk.Combobox(frame,font =("sans-serif", 20, "bold"), state="readonly",values=["volovan salado", "volovan dulce", "especial"])
+        self.txtcategoria.place(x=620, y=320, width=250)
+
+        #Login Button
+        createProductbtn=Button(frame,command=self.createProduct, text="Crear Producto",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        createProductbtn.place(x=230, y=400, width=140, height=35)
+
+        #Register Button
+        exitbtn=Button(frame,command=self.exit, text="Salir",font =("calibri", 15, "bold"),bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        exitbtn.place(x=530, y=400, width=140, height=35)
+
+    def exit(self): 
+        self.root.switch_frame(Admin_window)
+
+    def createProduct(self):
+        pass 
+
+#Agregando producción
+class Production_window(Frame):
+    def __init__(self, root):
+        self.root=root
+        #self.root.wm_attributes("-transparentcolor", 'black')
+        self.root.title("EL VOLOVAN")
+        self.root.geometry("1550x800+0+0")
+        bg = Image.open("img/background.jpg")
+        bg = bg.resize( (1550, 800), Image.ANTIALIAS)
+        self.bg = ImageTk.PhotoImage(bg)
+        lbl_bg  = Label(self.root, image= self.bg)
+        lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
+
+        Frame.__init__(self, root)
+        frame = Frame(self.root, bg="white")
+        frame.place(x=450, y=180, width= 600, height=500)
+
+
+        logo = Image.open("img/logo.png")
+        logo = logo.resize((200,200), Image.ANTIALIAS)
+        self.photoImage1= ImageTk.PhotoImage(logo)
+        lbllogo=Label(frame, image=self.photoImage1, bg="white", borderwidth=0)
+        lbllogo.place(x=20, y=20, width=200, height=200)
+
+
+
+        get_str = Label(frame, text="Producción", font =("sans-serif", 40, "bold"), fg="darkorange", bg="white")
+        get_str.place(x=250, y=50)
+        
+        #Label 
+
+        
+        nombre=Label(frame, text="Nombre", font =("sans-serif", 20, "bold"), fg="teal", bg="white")
+        nombre.place(x=100, y=240)
+
+        self.txtnombre = ttk.Combobox(frame, font =("sans-serif", 20, "bold"), state="readonly",values=[])
+        self.txtnombre.place(x=300, y=240, width=250)
+
+        cantidad=Label(frame, text="Cantidad", font =("sans-serif", 20, "bold"), fg="teal", bg="white")
+        cantidad.place(x=100, y=290)
+
+        self.txtcantidad = ttk.Entry(frame, font =("sans-serif", 20, "bold"))
+        self.txtcantidad.place(x=300, y=290, width=250)
+
+
+        #Login Button
+        exitbtn=Button(frame,command=self.exit, text="Salir",font =("calibri", 15, "bold"), bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        exitbtn.place(x=130, y=400, width=140, height=35)
+
+        #Register Button
+        aggProdbtn=Button(frame,command=self.agregarProduccion, text="Agregar",font =("calibri", 15, "bold"),bd=3, relief=RIDGE, fg="white", bg="lightseagreen")
+        aggProdbtn.place(x=330, y=400, width=140, height=35)
+
+    def agregarProduccion(self): 
+        pass
+        
+
+    def exit(self): 
+        self.root.switch_frame(Admin_window)
 
 class SampleApp(Tk):
     def __init__(self):
